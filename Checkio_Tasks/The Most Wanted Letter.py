271d55efd000baffe9d0c18b8e5dc3269dd5f2bd
esergly <kievbsm@gmail.com>
2019-09-12 12:29:53 +0300

    Add new tasks

--- Checkio_Tasks/The Most Wanted Letter.py
+++ Checkio_Tasks/The Most Wanted Letter.py
@@ -31,19 +31,16 @@ def checkio(text: str) -> str:
 
 
 if __name__ == '__main__':
-    #    print("Example:")
-    #    print(checkio("Hello World!"))
-
+    print("Example:")
+    print(checkio("Hello World!"))
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    #    assert checkio("Hello World!") == "l", "Hello test"
-    #    assert checkio("How do you do?") == "o", "O is most wanted"
-    #    assert checkio("One") == "e", "All letter only once."
-    #    assert checkio("Oops!") == "o", "Don't forget about lower case."
-    #    assert checkio("AAaooo!!!!") == "a", "Only letters."
-    #    assert checkio("abe") == "a", "The First."
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
     assert checkio("Lorem ipsum dolor sit amet") == "m", "m is most wanted"
-#    print("Start the long test")
-#    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
-#    print("The local tests are done.")
-print(checkio("Lorem ipsum dolor sit amet"))
-print(checkio("Lorem ipsum dolor sit amet"))
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are successfully done.")
